02dbaafa1d19b6bdf6ce04b0a48382a29cd52974
vanya010792 <vanya010792@gmail.com>
2019-05-30 16:44:48 +0300

    up

--- src/js/project/main.js
+++ src/js/project/main.js
@@ -9,5 +9,16 @@ document.addEventListener( 'DOMContentLoaded', function () {
             document.querySelector( '.sb-head__nav' ).classList.add( 'sb-head__nav--active' );
         }
     });
+    if( document.querySelector( '.sb-faq' ) ) {
+        $( '.sb-faq__title' ).click( function () {
+            let thisIndex = $( '.sb-faq__title' ).index( this );
+            for( let a = 0; a < $( '.sb-faq__title' ).length; a++ ) {
+                if( a !== thisIndex ) {
+                    $( '.sb-faq__title' ).eq(a).removeClass( 'sb-faq__title--active' );
+                }
+            }
+            $( this ).toggleClass('sb-faq__title--active');
+        });
+    }
 
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,3 @@
+//= jquery/jquery-3.3.1.min.js
+
 //= project/main.js
